09453ba0a154dbae05219dbd47e5cf7116913722
hanawazaki <hanawazaki002@gmail.com>
2020-12-11 17:14:26 +0700

    add route API, add update photo

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -7,7 +7,9 @@ use App\Helpers\ResponseFormatter;
 use App\Http\Controllers\Controller;
 use Exception;
 use Facade\FlareClient\Http\Response;
+use Illuminate\Contracts\Validation\Validator;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
@@ -91,6 +93,50 @@ class UserController extends Controller
     {
         $token = $request->user()->currentAccessToken()->delete();
 
-        return ResponseFormatter::success([$token,'Token Revoked']);
+        return ResponseFormatter::success([$token, 'Token Revoked']);
+    }
+
+    public function fetch(Request $request)
+    {
+        return ResponseFormatter::success([
+            $request->user(), 'Data Berhasil diambil'
+        ]);
+    }
+
+    public function updateProfile(Request $request)
+    {
+        $data = $request->all();
+
+        $user = Auth::User();
+        $user->update($data);
+
+        return ResponseFormatter::success([$user, "Profile Updated"]);
+    }
+
+    public function updatePhoto(Request $request)
+    {
+        $validator = Validator::make(
+            $request->all(),
+            [
+                'file' => 'required|image|max:2048'
+            ]
+        );
+
+        if ($validator->fails()) {
+            return ResponseFormatter::error([
+                'error' => $validator->errors()
+            ], 'Update Photo Fails', 401);
+        }
+
+        if ($request->file('file')) {
+            $file = $request->file->store('assets/user', 'public');
+
+            // save ke DB
+            $user = Auth::user();
+            $user->update_photo_path = $file;
+            $user->update();
+
+            return ResponseFormatter::success([$file], 'File Successfully Uploaded');
+        }
     }
 }
