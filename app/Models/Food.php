01abacd450d84b1f75f1b1308d7a99eef5a776ef
hanawazaki <hanawazaki002@gmail.com>
2020-12-11 14:56:44 +0700

    Initial commit

---
+++ app/Models/Food.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Food extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = [
+        'name',
+        'ingridient',
+        'price',
+        'rate',
+        'types',
+        'description',
+        'picture_path'
+    ];
+
+    public function getCreatedAtAttribute($value){
+        return Carbon::parse($value)->timestamp;
+    }
+
+    public function getUpdatedAtAttribute($value){
+        return Carbon::parse($value)->timestamp;
+    }
+
+    // public function toArray()
+    // {
+    //     $toarray = parent::toArray();
+    //     $toarray['picture_path']=$this->picture_path;
+    //     return $toarray;
+    // }
+
+    public function getPicturePathAttribute(){
+        return url('') . Storage::url($this->attributes['picture_path']);
+    }
+}
---
+++ app/Models/Transaction.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Transaction extends Model
+{
+    use HasFactory;
+    use SoftDeletes;
+
+    protected $fillable = [
+        'user_id',
+        'food_id',
+        'total',
+        'quantity',
+        'status',
+        'payment_url'
+    ];
+
+    public function getCreatedAtAttribute($value){
+        return Carbon::parse($value)->timestamp;
+    }
+
+    public function getUpdatedAtAttribute($value){
+        return Carbon::parse($value)->timestamp;
+    }
+
+    public function food(){
+        $this->HasOne(Food::class,'id','food_id');
+    }
+
+    public function user(){
+        $this->HasOne(User::class,'id','user_id');
+    }
+
+
+}
